f7eaad10f62cca8fea627021952c679e3bd5253f
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-04-17 12:52:10 +0300

    Added Spring Boot 2.x nature

---
+++ src/test/java/gr/codehub/guide/filmrepository/FilmRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package gr.codehub.guide.filmrepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilmRepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
