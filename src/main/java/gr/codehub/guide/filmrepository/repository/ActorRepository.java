ebaca46dc936acc02a03e80aead1bdaab605b867
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-09 17:21:53 +0300

    Exhibition of several JPA retrieval techniques.

--- src/main/java/gr/codehub/guide/filmrepository/repository/ActorRepository.java
+++ src/main/java/gr/codehub/guide/filmrepository/repository/ActorRepository.java
@@ -1,10 +1,20 @@
 package gr.codehub.guide.filmrepository.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import gr.codehub.guide.filmrepository.model.Actor;
 
 @Repository
 public interface ActorRepository extends JpaRepository<Actor, Long> {
+	/*
+	DON'T return List<ActorFilmsPair>, just List. This is a bug introduced in Spring Data JPA 2.0.5
+		Check this https://stackoverflow.com/questions/49056084/got-different-size-of-tuples-and-aliases-exception-after-spring-boot-2-0-0-rel?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+		and https://jira.spring.io/browse/DATAJPA-1280 for more details
+	 */
+	@Query(name = "Actor.getNumOfFilmsPerActor", nativeQuery = true)
+	List getNumOfFilmsPerActor();
 }
