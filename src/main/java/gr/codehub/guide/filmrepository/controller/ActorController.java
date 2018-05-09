9116db2d34daec9bc114e317c02892a5bba0e5b0
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-09 17:23:35 +0300

    REST endpoints exposing data loaded via Spring Data JPA.

--- src/main/java/gr/codehub/guide/filmrepository/controller/ActorController.java
+++ src/main/java/gr/codehub/guide/filmrepository/controller/ActorController.java
@@ -1,12 +1,16 @@
 package gr.codehub.guide.filmrepository.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gr.codehub.guide.filmrepository.model.Actor;
 import gr.codehub.guide.filmrepository.service.AbstractDomainService;
 import gr.codehub.guide.filmrepository.service.ActorService;
+import gr.codehub.guide.filmrepository.transfer.ActorFilmsPair;
 
 @RestController
 @RequestMapping("/actors")
@@ -18,5 +22,10 @@ public class ActorController extends AbstractDomainController<Actor> {
 	@Override
 	public AbstractDomainService<Actor> getDomainService() {
 		return service;
+	}
+
+	@GetMapping(headers = "action=getNumOfFilmsPerActor")
+	public List<ActorFilmsPair> getNumOfFilmsPerActor() {
+		return service.getNumOfFilmsPerActor();
 	}
 }
--- src/main/java/gr/codehub/guide/filmrepository/controller/FilmController.java
+++ src/main/java/gr/codehub/guide/filmrepository/controller/FilmController.java
@@ -1,12 +1,17 @@
 package gr.codehub.guide.filmrepository.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gr.codehub.guide.filmrepository.model.Film;
 import gr.codehub.guide.filmrepository.service.AbstractDomainService;
 import gr.codehub.guide.filmrepository.service.FilmService;
+import gr.codehub.guide.filmrepository.transfer.FilmActorPair;
+import gr.codehub.guide.filmrepository.transfer.KeyValue;
 
 @RestController
 @RequestMapping("/films")
@@ -17,5 +22,15 @@ public class FilmController extends AbstractDomainController<Film> {
 	@Override
 	public AbstractDomainService<Film> getDomainService() {
 		return service;
+	}
+
+	@GetMapping(headers = "action=getTitles")
+	public List<KeyValue<Long, String>> getTitles() {
+		return service.getTitles();
+	}
+
+	@GetMapping(headers = "action=getNumOfActorsPerFilm")
+	public List<FilmActorPair> getNumOfActorsPerFilm() {
+		return service.getNumOfActorsPerFilm();
 	}
 }
