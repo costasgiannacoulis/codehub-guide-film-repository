e80a4979b33f5ef4ff923543344b7398bfee783c
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-11 13:59:59 +0300

    Enabled Spring Security 5.

--- src/main/java/gr/codehub/guide/filmrepository/config/BasicAuthenticationPoint.java
+++ src/main/java/gr/codehub/guide/filmrepository/config/BasicAuthenticationPoint.java
@@ -3,7 +3,6 @@ package gr.codehub.guide.filmrepository.config;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
