b731944de25829b2c25eed983f8c4a47a77f446c
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-11 13:58:51 +0300

    Enabled Spring Security 5.

---
+++ src/main/java/gr/codehub/guide/filmrepository/config/BasicAuthenticationPoint.java
@@ -0,0 +1,31 @@
+package gr.codehub.guide.filmrepository.config;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BasicAuthenticationPoint extends BasicAuthenticationEntryPoint {
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		setRealmName("Film-Realm");
+		super.afterPropertiesSet();
+	}
+
+	@Override
+	public void commence(
+		final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authEx)
+		throws IOException {
+		response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		final PrintWriter writer = response.getWriter();
+		writer.println("HTTP Status 401 - " + authEx.getMessage());
+	}
+}
