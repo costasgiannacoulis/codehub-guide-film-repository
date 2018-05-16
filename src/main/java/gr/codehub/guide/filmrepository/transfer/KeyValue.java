41c0dbbb313d6f8bfd39891443c7fbe1ad2f478a
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-16 23:21:08 +0300

    Added javadocs.

--- src/main/java/gr/codehub/guide/filmrepository/transfer/KeyValue.java
+++ src/main/java/gr/codehub/guide/filmrepository/transfer/KeyValue.java
@@ -2,6 +2,9 @@ package gr.codehub.guide.filmrepository.transfer;
 
 import lombok.Value;
 
+/**
+ * This Data Transfer Object is a generic key-value POJO.
+ */
 @Value
 public class KeyValue<K, V> {
 	K key;
