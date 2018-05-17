808952d0d81506ee2afd57c52e44f9120798d4b3
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-17 13:24:50 +0300

    Modified controller layer to create location header during resource creation and return specific http status during update and delete actions.

--- src/main/java/gr/codehub/guide/filmrepository/controller/AbstractDomainController.java
+++ src/main/java/gr/codehub/guide/filmrepository/controller/AbstractDomainController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,20 +13,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import gr.codehub.guide.filmrepository.service.AbstractDomainService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class AbstractDomainController<T> {
+	public abstract AbstractDomainService<T> getDomainService();
+
 	@GetMapping("/{id}")
 	public T get(@PathVariable("id") final Long id) {
 		final T entity = getDomainService().get(id);
-		log.debug(entity.toString());
+		log.debug("Returning as REST output: " + entity.toString());
 		return entity;
 	}
-
-	public abstract AbstractDomainService<T> getDomainService();
 
 	@GetMapping
 	public List<T> findAll() {
@@ -32,11 +35,13 @@ public abstract class AbstractDomainController<T> {
 	}
 
 	@PostMapping
-	public void create(@Valid @RequestBody final T entity) {
-		getDomainService().create(entity);
+	public ResponseEntity<T> create(@Valid @RequestBody final T entity) {
+		final T savedEntity = getDomainService().create(entity);
+		return new ResponseEntity<>(savedEntity, HttpStatus.CREATED);
 	}
 
 	@PutMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void update(@Valid @RequestBody final T entity) {
 		if (getDomainService().exists(entity)) {
 			getDomainService().update(entity);
@@ -44,11 +49,13 @@ public abstract class AbstractDomainController<T> {
 	}
 
 	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") final Long id) {
 		getDomainService().delete(id);
 	}
 
 	@DeleteMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@Valid @RequestBody final T entity) {
 		if (getDomainService().exists(entity)) {
 			getDomainService().delete(entity);
