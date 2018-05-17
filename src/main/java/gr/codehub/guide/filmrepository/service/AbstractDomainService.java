e75fa217567ed9fb8ae12983b3c136cfcd4762fc
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-17 13:05:53 +0300

    Create method should return the newly created object in order to set location header containing corresponding URI.

--- src/main/java/gr/codehub/guide/filmrepository/service/AbstractDomainService.java
+++ src/main/java/gr/codehub/guide/filmrepository/service/AbstractDomainService.java
@@ -3,7 +3,7 @@ package gr.codehub.guide.filmrepository.service;
 import java.util.List;
 
 public interface AbstractDomainService<T> {
-	void create(final T entity);
+	T create(final T entity);
 
 	void update(T entity);
 
