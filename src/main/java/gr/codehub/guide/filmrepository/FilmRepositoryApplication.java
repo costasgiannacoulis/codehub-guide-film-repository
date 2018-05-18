de8b83fa48b66f799112bf112cc6fe560af0f332
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-18 16:26:29 +0300

    Added internationalization showcases.

--- src/main/java/gr/codehub/guide/filmrepository/FilmRepositoryApplication.java
+++ src/main/java/gr/codehub/guide/filmrepository/FilmRepositoryApplication.java
@@ -1,7 +1,14 @@
 package gr.codehub.guide.filmrepository;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 /**
  * Application launcher class.
@@ -15,5 +22,43 @@ public class FilmRepositoryApplication {
 	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(FilmRepositoryApplication.class, args);
+	}
+
+	/**
+	 * Instantiates a session locale resolver with US locale being the default locale.
+	 *
+	 * @return the instantiated resolver.
+	 */
+	//@Bean
+	public LocaleResolver localeResolver() {
+		final SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
+	}
+
+	/**
+	 * Instantiates an accept header locale resolver with US locale being the default locale.
+	 *
+	 * @return the instantiated resolver.
+	 */
+	@Bean
+	public LocaleResolver acceptHeaderLocaleResolver() {
+		final AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
+	}
+
+	/**
+	 * Instantiates a message source bean defaulting to messages.properties
+	 *
+	 * @return The resource bundle.
+	 */
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
