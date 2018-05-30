9105c089402bb2202c84461856792a276a943983
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-05-30 12:38:53 +0300

    Added javadocs.

--- src/main/java/gr/codehub/guide/filmrepository/controller/LanguageController.java
+++ src/main/java/gr/codehub/guide/filmrepository/controller/LanguageController.java
@@ -16,18 +16,35 @@ import gr.codehub.guide.filmrepository.model.Language;
 import gr.codehub.guide.filmrepository.service.AbstractDomainService;
 import gr.codehub.guide.filmrepository.service.LanguageService;
 
+/**
+ * Controller serving {@link Language} related requests.
+ */
 @RestController
 @RequestMapping("/languages")
 public class LanguageController extends AbstractDomainController<Language> {
 
+	/**
+	 * Service providing access to {@link Language} specific business calls.
+	 */
 	@Autowired
 	LanguageService service;
 
+	/**
+	 * Declaring service implementation in order to support the execution of common actions.
+	 *
+	 * @return The business service.
+	 */
 	@Override
 	public AbstractDomainService<Language> getDomainService() {
 		return service;
 	}
 
+	/**
+	 * Override default method for creating an {@link Language} object in order to demonstrate URL rewriting.
+	 * @param entity The entity's body without id.
+
+	 * @return the persisted {@link Language} object.
+	 */
 	@Override
 	@PostMapping
 	public ResponseEntity create(@Valid @RequestBody final Language entity) {
