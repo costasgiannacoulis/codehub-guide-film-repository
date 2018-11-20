75c235efb194d8639b527077aeef7a1e01913644
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-11-20 10:24:33 +0200

    Remove loop in calling asynchronously service method to speed up bootstrap.

--- src/main/java/gr/codehub/guide/filmrepository/bootstrap/AsyncExhibitor.java
+++ src/main/java/gr/codehub/guide/filmrepository/bootstrap/AsyncExhibitor.java
@@ -43,29 +43,29 @@ public class AsyncExhibitor implements ApplicationRunner {
 		log.info("Starting asynchronous exhibition...");
 		final LocalDateTime beginning = LocalDateTime.now();
 
-		for (int i = 1; i <= 10; i++) {
-			final CompletableFuture<List<Film>> films = asyncGroupService.findAllFilms();
-			final CompletableFuture<List<FilmActorPair>> actorsPerFilm = asyncGroupService.getNumOfActorsPerFilm();
-			final CompletableFuture<List<KeyValue<Long, String>>> filmTitles = asyncGroupService.getFilmTitles();
-			final CompletableFuture<List<Actor>> actors = asyncGroupService.findAllActors();
-			final CompletableFuture<List<Category>> categories = asyncGroupService.findAllCategories();
-			final CompletableFuture<List<Language>> languages = asyncGroupService.findAllLanguages();
-			final CompletableFuture<List<Film>> films2 = asyncGroupService.findAllFilms();
-			final CompletableFuture<List<FilmActorPair>> actorsPerFilm2 = asyncGroupService.getNumOfActorsPerFilm();
-			final CompletableFuture<List<KeyValue<Long, String>>> filmTitles2 = asyncGroupService.getFilmTitles();
-			//CompletableFuture.allOf(actors, categories, films, languages).join();
+		final CompletableFuture<List<Film>> films = asyncGroupService.findAllFilms();
+		final CompletableFuture<List<FilmActorPair>> actorsPerFilm = asyncGroupService.getNumOfActorsPerFilm();
+		final CompletableFuture<List<KeyValue<Long, String>>> filmTitles = asyncGroupService.getFilmTitles();
+		final CompletableFuture<List<Actor>> actors = asyncGroupService.findAllActors();
+		final CompletableFuture<List<Category>> categories = asyncGroupService.findAllCategories();
+		final CompletableFuture<List<Language>> languages = asyncGroupService.findAllLanguages();
+		final CompletableFuture<List<Film>> films2 = asyncGroupService.findAllFilms();
+		final CompletableFuture<List<FilmActorPair>> actorsPerFilm2 = asyncGroupService.getNumOfActorsPerFilm();
+		final CompletableFuture<List<KeyValue<Long, String>>> filmTitles2 = asyncGroupService.getFilmTitles();
 
-			log.info("Actors found: " + actors.get().size());
-			log.info("Categories found: " + categories.get().size());
-			log.info("Actors per films found: " + actorsPerFilm.get().size());
-			log.info("Films found: " + films.get().size());
-			log.info("Film titles found: " + filmTitles.get().size());
-			log.info("Languages found: " + languages.get().size());
-			log.info("Repeating some film related queries");
-			log.info("Actors per films found: " + actorsPerFilm2.get().size());
-			log.info("Films found: " + films2.get().size());
-			log.info("Film titles found: " + filmTitles2.get().size());
-		}
+		// Wait for all async methods to return
+		//CompletableFuture.allOf(actors, categories, films, languages).join();
+
+		log.info("Actors found: " + actors.get().size());
+		log.info("Categories found: " + categories.get().size());
+		log.info("Actors per films found: " + actorsPerFilm.get().size());
+		log.info("Films found: " + films.get().size());
+		log.info("Film titles found: " + filmTitles.get().size());
+		log.info("Languages found: " + languages.get().size());
+		log.info("Repeating some film related queries");
+		log.info("Actors per films found: " + actorsPerFilm2.get().size());
+		log.info("Films found: " + films2.get().size());
+		log.info("Film titles found: " + filmTitles2.get().size());
 
 		log.info("Finished asynchronous exhibition in {} seconds.",
 			Duration.between(beginning, LocalDateTime.now()).getSeconds());
