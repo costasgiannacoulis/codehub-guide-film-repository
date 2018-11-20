25fd5eb20824373800e90f4d557b12ef63af34bd
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-11-20 10:40:37 +0200

    Initial commit about Swagger. Demonstration using Actor related classes.

---
+++ src/main/java/gr/codehub/guide/filmrepository/config/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package gr.codehub.guide.filmrepository.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger 2 configuration
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+	/**
+	 * Swagger documentation directives
+	 *
+	 * @return the directives set
+	 */
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis(RequestHandlerSelectors.basePackage("gr.codehub.guide.filmrepository.controller"))
+			.paths(PathSelectors.any())
+			.build().apiInfo(getApiInfo());
+	}
+
+	/**
+	 * Some API info
+	 *
+	 * @return the API info
+	 */
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder()
+			.title("Swagger API Doc")
+			.description("More description about the API")
+			.version("1.0.0")
+			.build();
+	}
+
+	/**
+	 * Some overrides while trying to find why Swagger-UI does not work
+	 *
+	 * @param registry the updated registry
+	 */
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry
+			.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
--- src/main/java/gr/codehub/guide/filmrepository/controller/ActorController.java
+++ src/main/java/gr/codehub/guide/filmrepository/controller/ActorController.java
@@ -23,6 +23,7 @@ import gr.codehub.guide.filmrepository.model.Actor;
 import gr.codehub.guide.filmrepository.service.AbstractDomainService;
 import gr.codehub.guide.filmrepository.service.ActorService;
 import gr.codehub.guide.filmrepository.transfer.ActorFilmsPair;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * Controller serving {@link Actor} related requests.
@@ -60,6 +61,8 @@ public class ActorController extends AbstractDomainController<Actor> {
 	 */
 	@Override
 	@PostMapping
+	@ApiOperation(value = "Create an Actor", notes = "Creates a new Actor entity based on a validated input JSON " +
+		"document")
 	public ResponseEntity create(@Valid @RequestBody final Actor entity) {
 		final Actor savedEntity = getDomainService().create(entity);
 		final URI location = ServletUriComponentsBuilder
